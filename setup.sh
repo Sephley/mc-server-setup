db1f4e1b2bb53749c381cb6be8a4559ca29fa384
Sephley <hurley.joseph@protonmail.ch>
2023-06-09 11:01:10 +0200

    Update dependencies and accept eula

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ readonly RED='\033[0;31m'   # Red
 readonly GREEN='\u001b[32m' # Green
 readonly NC='\033[0m'       # Neutral (White)
 
-# Server download URLs
+# Server download URLs | see https://mcversions.net/ for other versions
 readonly 1.19.4='https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar'
 
 get_dependencies () {
@@ -17,16 +17,20 @@ get_dependencies () {
    if [ -f /etc/apt/sources.list ]; then
         apt update
         apt -y install openjdk-17-jre-headless
+        apt -y install screen
    elif [-f /etc/yum.conf ]; then
         yum -y install openjdk-17-jre-headless
+        yum -y install screen
    elif [-f /etc/pacman.conf ]; then
         pacman -Sy
         pacman -S --noconfirm pacman
         pacman -S --noconfirm openjdk-17-jre-headless
+        pacman -S --noconfirm screen
    else
    echo -e "$RED""Your distribution is not supported by this StackScript""$NC"
-   exit
+   exit 0
    fi
+}
 
 enable_port () {
   sudo ufw enable 25565
@@ -34,7 +38,21 @@ enable_port () {
 
 prepare_workspace () {
   sudo mkdir /srv/minecraft
-  
-  # see https://mcversions.net/ for other versions
   sudo wget "$1.19.4" -O /srv/minecraft/server.jar
+  java -Xms2048M -Xmx4096M -jar /srv/minecraft/server.jar nogui
 }
+
+enable_eula () {
+   rm /srv/minecraft/eula.txt
+   touch /srv/minecraft/eula.txt
+   echo "eula=true" > /srv/minecraft/eula.txt
+}
+
+main () {
+   get_dependencies
+   enable_port
+   prepare_workspace
+   enable_eula
+}
+
+main
